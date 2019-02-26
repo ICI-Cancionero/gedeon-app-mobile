1b43f7602252d62b6da751e957a7bd5947c7937c
Luis Porras <lporras16@gmail.com>
2019-02-26 10:34:39 -0300

    adding SongsTab component

--- App.js
+++ App.js
@@ -1,76 +1,19 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
-import SongsList from './components/SongsList';
-import { styles } from './components/Styles';
+import SongsTab from './components/SongsTab';
 import { Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 //import { Container, Content, Header, Footer, FooterTab, Button, Text } from 'native-base';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true, songsDB: [], component: 0};
-    this.getSongsTitlesFromInitial = this.getSongsTitlesFromInitial.bind(this);
-    this.getSongsSection = this.getSongsSection.bind(this);
-    this.fetchData = this.fetchData.bind(this);
-  }
-  getSongsTitlesFromInitial (initial) {
-    return this.state.songsDB.filter((song) => {
-      return song.title.charAt(0) == initial
-    })
-  }
-
-  getSongsSection () {
-    let initials = this.state.songsDB.map((song) => {
-      return song.title.charAt(0);
-    });
-    let uniqueInitials = [...new Set(initials)].sort();
-    return uniqueInitials.map((initial) => {
-      return {
-        initial: initial,
-        data: this.getSongsTitlesFromInitial(initial)
-      }
-    });
-  }
-
-  async fetchData() {
-    let songs = [];
-    try {
-      let response = await fetch(
-        'https://gedeon-app-rails.herokuapp.com/api/v1/songs'
-      );
-      songs = await response.json();
-    } catch (error) {
-      console.log(error);
-    }
-    if (songs.length > 0) {
-      this.setState({
-        isLoading: false,
-        songsDB: songs
-      });
-    }
-  }
-
-  componentDidMount(){
+  componentDidMount () {
     Font.loadAsync({
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
     });
-    this.fetchData().done();
   }
 
   render() {
-    if (this.state.isLoading) {
-      return(
-        <View style={{flex: 1, padding: 40}}>
-          <ActivityIndicator/>
-        </View>
-      );
-    } else {
-      return (
-        <SongsList songsSection={this.getSongsSection()} />
-      );
-    }
+    return(<SongsTab />);
   }
 };
---
+++ components/SongsTab.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import SongsList from './SongsList';
+
+export default class SongsTab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true, songsDB: [], component: 0};
+    this.getSongsTitlesFromInitial = this.getSongsTitlesFromInitial.bind(this);
+    this.getSongsSection = this.getSongsSection.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+  }
+  getSongsTitlesFromInitial (initial) {
+    return this.state.songsDB.filter((song) => {
+      return song.title.charAt(0) == initial
+    })
+  }
+
+  getSongsSection () {
+    let initials = this.state.songsDB.map((song) => {
+      return song.title.charAt(0);
+    });
+    let uniqueInitials = [...new Set(initials)].sort();
+    return uniqueInitials.map((initial) => {
+      return {
+        initial: initial,
+        data: this.getSongsTitlesFromInitial(initial)
+      }
+    });
+  }
+
+  async fetchData() {
+    let songs = [];
+    try {
+      let response = await fetch(
+        'https://gedeon-app-rails.herokuapp.com/api/v1/songs'
+      );
+      songs = await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+    if (songs.length > 0) {
+      this.setState({
+        isLoading: false,
+        songsDB: songs
+      });
+    }
+  }
+
+  componentDidMount(){
+    this.fetchData().done();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return(
+        <View style={{flex: 1, padding: 40}}>
+          <ActivityIndicator/>
+        </View>
+      );
+    } else {
+      return (
+        <SongsList songsSection={this.getSongsSection()} />
+      );
+    }
+  }
+};
