90b6634ae67c2678aa74b0e39eab67d6ae3c1af8
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-06-23 08:44:42 -0500

    using function App instead of class

--- App.js
+++ App.js
@@ -13,58 +13,57 @@ const renderTabBar = (props) => {
   return <DefaultTabBar {...props} />;
 };
 
-export default class App extends React.Component {
+async function _loadFonts() {
+  await Font.loadAsync({
+    'Roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    ...Ionicons.font,
+  });
+}
+
+export default function App () {
 
   state = {
     isReady: false
   }
 
-  async _loadFonts() {
-    await Font.loadAsync({
-      'Roboto': require('./assets/fonts/Roboto-Regular.ttf'),
-      'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf'),
-      ...Ionicons.font,
-    });
-  }
 
-  render() {
-    if (!this.state.isReady) return (
-      <AppLoading
-        startAsync={this._loadFonts}
-        onFinish={() => this.setState({isReady: true})}
-        onError={console.warn}
-      />
-    )
+  if (!this.state.isReady) return (
+    <AppLoading
+      startAsync={this._loadFonts}
+      onFinish={() => this.setState({isReady: true})}
+      onError={console.warn}
+    />
+  )
 
-    return(
-      <Container>
-        <Header hasTabs style={styles.header}>
-          <Body style={{flex: 1}}>
-            <Title style={{color: 'white', alignSelf: 'center'}}>Cancionero del Reino</Title>
-          </Body>
-        </Header>
-        <Tabs
-          renderTabBar={renderTabBar}
-          tabBarUnderlineStyle={styles.tabBarUnderline}
+  return(
+    <Container>
+      <Header hasTabs style={styles.header}>
+        <Body style={{flex: 1}}>
+          <Title style={{color: 'white', alignSelf: 'center'}}>Cancionero del Reino</Title>
+        </Body>
+      </Header>
+      <Tabs
+        renderTabBar={renderTabBar}
+        tabBarUnderlineStyle={styles.tabBarUnderline}
+      >
+        <Tab
+          heading={<TabHeading style={styles.tabHeading}>
+            <Text style={{color: colors.blackPurple}}>Canciones</Text>
+          </TabHeading>}
+          activeTextStyle={styles.activeText}
         >
-          <Tab
-            heading={<TabHeading style={styles.tabHeading}>
-              <Text style={{color: colors.blackPurple}}>Canciones</Text>
-            </TabHeading>}
-            activeTextStyle={styles.activeText}
-          >
-            <SongsTab />
-          </Tab>
-          <Tab
-            heading={<TabHeading style={styles.tabHeading}>
-              <Text style={{color: colors.blackPurple}}>Listas</Text>
-            </TabHeading>}
-            activeTextStyle={styles.activeText}
-          >
-            <ListsTab />
-          </Tab>
-        </Tabs>
-      </Container>
-    );
-  }
+          <SongsTab />
+        </Tab>
+        <Tab
+          heading={<TabHeading style={styles.tabHeading}>
+            <Text style={{color: colors.blackPurple}}>Listas</Text>
+          </TabHeading>}
+          activeTextStyle={styles.activeText}
+        >
+          <ListsTab />
+        </Tab>
+      </Tabs>
+    </Container>
+  );
 };
