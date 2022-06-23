1d4447d45525f453852af3d1bfa757337f3cde15
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-06-23 08:58:11 -0500

    using SplashScreen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import "./ignoreWarnings";
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SongsTab from './components/SongsTab';
 import ListsTab from './components/ListsTab';
-import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { Ionicons } from '@expo/vector-icons';
-import { Body, Container, Content, Header, Tab, Tabs, Text, Title, DefaultTabBar, TabHeading } from 'native-base';
+import { Body, Container, Content, Header, Tab, Tabs, Text, Title, DefaultTabBar, TabHeading, View } from 'native-base';
 import { colors, styles } from './components/Styles';
 
 const renderTabBar = (props) => {
@@ -13,57 +13,79 @@ const renderTabBar = (props) => {
   return <DefaultTabBar {...props} />;
 };
 
-async function _loadFonts() {
-  await Font.loadAsync({
-    'Roboto': require('./assets/fonts/Roboto-Regular.ttf'),
-    'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf'),
-    ...Ionicons.font,
-  });
-}
-
 export default function App () {
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  state = {
-    isReady: false
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'Roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+          'Roboto_medium': require('./assets/fonts/Roboto-Medium.ttf'),
+          ...Ionicons.font,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
-
-  if (!this.state.isReady) return (
-    <AppLoading
-      startAsync={this._loadFonts}
-      onFinish={() => this.setState({isReady: true})}
-      onError={console.warn}
-    />
-  )
-
-  return(
-    <Container>
-      <Header hasTabs style={styles.header}>
-        <Body style={{flex: 1}}>
-          <Title style={{color: 'white', alignSelf: 'center'}}>Cancionero del Reino</Title>
-        </Body>
-      </Header>
-      <Tabs
-        renderTabBar={renderTabBar}
-        tabBarUnderlineStyle={styles.tabBarUnderline}
-      >
-        <Tab
-          heading={<TabHeading style={styles.tabHeading}>
-            <Text style={{color: colors.blackPurple}}>Canciones</Text>
-          </TabHeading>}
-          activeTextStyle={styles.activeText}
+  return (
+    <View
+      onLayout={onLayoutRootView}
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Container>
+        <Header hasTabs style={styles.header}>
+          <Body style={{flex: 1}}>
+            <Title style={{color: 'white', alignSelf: 'center'}}>Cancionero del Reino</Title>
+          </Body>
+        </Header>
+        <Tabs
+          renderTabBar={renderTabBar}
+          tabBarUnderlineStyle={styles.tabBarUnderline}
         >
-          <SongsTab />
-        </Tab>
-        <Tab
-          heading={<TabHeading style={styles.tabHeading}>
-            <Text style={{color: colors.blackPurple}}>Listas</Text>
-          </TabHeading>}
-          activeTextStyle={styles.activeText}
-        >
-          <ListsTab />
-        </Tab>
-      </Tabs>
-    </Container>
+          <Tab
+            heading={<TabHeading style={styles.tabHeading}>
+              <Text style={{color: colors.blackPurple}}>Canciones</Text>
+            </TabHeading>}
+            activeTextStyle={styles.activeText}
+          >
+            <SongsTab />
+          </Tab>
+          <Tab
+            heading={<TabHeading style={styles.tabHeading}>
+              <Text style={{color: colors.blackPurple}}>Listas</Text>
+            </TabHeading>}
+            activeTextStyle={styles.activeText}
+          >
+            <ListsTab />
+          </Tab>
+        </Tabs>
+      </Container>
+    </View>
   );
 };
